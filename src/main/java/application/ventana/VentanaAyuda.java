c21bd76301114f460bb21a21576ed5f252253c24
JairoPerte <jairopertecarras@gmail.com>
2024-05-07 10:15:22 +0200

    manual de ayuda con las cosas que dice el profesor

--- src/main/java/application/ventana/VentanaAyuda.java
+++ src/main/java/application/ventana/VentanaAyuda.java
@@ -1,11 +1,12 @@
 package application.ventana;
 
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -19,18 +20,39 @@ public class VentanaAyuda extends Stage {
 		VBox vbox = new VBox(5);
 
 		Label contacta = new Label("Para más ayuda dirigete a nuestra páigna web");
-		contacta.setAlignment(Pos.CENTER);
+		contacta.setTextAlignment(TextAlignment.CENTER);
+
+		Label copyright = new Label("Copyright © by RickyRack All rights reserved");
+		copyright.setTextAlignment(TextAlignment.CENTER);
+
+		Hyperlink link = new Hyperlink("GitHub público de RickyRack");
+		link.setTextAlignment(TextAlignment.CENTER);
+
+		link.setOnMouseClicked(event -> {
+			try {
+				String os = System.getProperty("os.name").toLowerCase();
+				if (os.contains("win")) {
+					Runtime.getRuntime().exec("cmd /c start https://github.com/JairoPerte/RickyRack");
+				} else if (os.contains("mac")) {
+					Runtime.getRuntime().exec("open https://github.com/JairoPerte/RickyRack");
+				} else if (os.contains("nix") || os.contains("nux")) {
+					Runtime.getRuntime().exec("xdg-open https://github.com/JairoPerte/RickyRack");
+				}
+			} catch (Exception e) {
+			}
+		});
 
 		WebView webView = new WebView();
 		WebEngine webEngine = webView.getEngine();
 
 		Button javadocPublic = new Button("Métodos Públicos");
+
 		Button javadocPrivate = new Button("Métodos Privados");
 
-		vbox.getChildren().addAll(contacta, javadocPublic, javadocPrivate);
+		vbox.getChildren().addAll(contacta, copyright, link, javadocPublic, javadocPrivate);
 
 		this.setTitle("Manual de Ayuda");
-		this.setScene(new Scene(vbox, 180, 100));
+		this.setScene(new Scene(vbox, 270, 140));
 		this.show();
 
 		javadocPublic.setOnMouseClicked(event -> {
